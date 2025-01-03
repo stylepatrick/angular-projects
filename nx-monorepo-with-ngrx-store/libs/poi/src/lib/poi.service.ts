c92d084158a294dbe53764656e47f07ac52b767f
stylepatrick <ahrntal.patrick@gmail.com>
2025-01-03 09:11:34 +0100

    add nx-monorepo-with-ngrx-store

---
+++ nx-monorepo-with-ngrx-store/libs/poi/src/lib/poi.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { PoiEntity } from '..';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PoiService {
+
+  constructor(private http: HttpClient) {}
+
+  getAll(): Observable<PoiEntity[]> {
+    console.log("init pois service")
+    return this.http.get<PoiEntity[]>('assets/poi.json');
+  }
+}
